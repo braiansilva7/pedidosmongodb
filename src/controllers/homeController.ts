281726538c9f986f6db8b58a2bc9691bf96e901c
braiansilva7 <braian.diogenes@gmail.com>
2022-08-04 16:10:19 -0300

    Terceira Atualização

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -4,11 +4,10 @@ import User from "../models/User";
 export const home = async (req: Request, res: Response)=>{
     let dataHoje = Intl.DateTimeFormat().format(new Date());
 
-    let usuarios = await User.find({});
-    
-    console.log("USUÁRIOS: "+ usuarios);
+    let solicitacao = await User.find({});
 
     res.render('pages/home', {
-        dataHoje
+        dataHoje,
+        solicitacao
     });
 }
