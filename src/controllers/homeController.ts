38aa579226c9c9ba2761510850912d560389fc46
braiansilva7 <braian.diogenes@gmail.com>
2022-08-04 17:43:34 -0300

    Botão ADD e Remover OK

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -11,3 +11,25 @@ export const home = async (req: Request, res: Response)=>{
         solicitacao
     });
 }
+
+export const criarPedido = async (req: Request, res: Response)=>{
+    let dataHoje: string = Intl.DateTimeFormat().format(new Date());
+    const pedido: string = req.body.solicitacao;
+    const cliente: string = req.body.cliente;
+    const contato: string = req.body.contato;
+
+    await User.create({
+        data: `${dataHoje}`,
+        pedido: `${pedido}`,
+        cliente: `${cliente}`,
+        contato: `${contato}`
+    });
+
+    res.redirect('/');
+}
+export const excluirPedido = async (req: Request, res: Response) =>{
+    const pegaId = req.params.id;
+    await User.deleteOne({id: pegaId});
+
+    res.redirect('/');
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,7 @@ import * as HomeController from "../controllers/homeController";
 const router = Router();
 
 router.get('/', HomeController.home);
+router.post('/novopedido', HomeController.criarPedido);
+router.get('/pedido/:id/excluir', HomeController.excluirPedido);
 
 export default router;
